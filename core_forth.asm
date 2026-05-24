e55d20df2faa1138415f2b7a066c8656542a8474
mknorman <mknorman@gmail.com>
2026-05-24 12:08:08 -0400

    demo echo loop

--- core_forth.asm
+++ core_forth.asm
@@ -3,16 +3,30 @@ section .rodata
 section .data
     hello db "Hello, World!", 10, 0
 section .bss
+    stdin        resq 1
+    input_buffer resb 256
 
 section .text
     global main
+    extern __acrt_iob_func
+    extern fgets
     extern printf
 main:
     push rbp
     mov rbp, rsp
     sub rsp, 32
-    mov rcx, hello
-    call printf
+    mov rcx, 0
+    call __acrt_iob_func
+    mov [rel stdin], rax
+
+    .loop:
+        lea rcx, [rel input_buffer]
+        mov rdx, 256
+        mov r8, [rel stdin]
+        call fgets
+        lea rcx, [rel input_buffer]
+        call printf
+        jmp .loop
     add rsp, 32
     mov rsp, rbp
     pop rbp
